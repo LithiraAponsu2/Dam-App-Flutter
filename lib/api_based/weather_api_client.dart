a26ebb730814bcd6c9e4e959ebbbc328772791d3
Lithira Aponsu <95391677+LithiraAponsu2@users.noreply.github.com>
2022-08-13 11:05:13 +0530

    Update weather_api_client.dart
    Removed unwanted parts

--- lib/api_based/weather_api_client.dart
+++ lib/api_based/weather_api_client.dart
@@ -6,7 +6,7 @@ import './weather.dart';
 class WeatherApiClient {
   Future<Weather>? getCurrentWeather() async {
     var endpoint = Uri.parse(
-        "https://api.openweathermap.org/data/2.5/weather?lat=7.0606&lon=80.5970&appid=b7177e3afee18daf9ff7d0a70f5bf381");
+        "https://api.openweathermap.org/data/2.5/weather?lat={your_lat}&lon={your_long}&appid={your_app_id}");
 
     var response = await http.get(endpoint);
     var body = jsonDecode(response.body);
